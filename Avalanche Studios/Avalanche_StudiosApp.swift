e7419ad1d986a6f6b372edab699cdf1265b9a9b5
manyworldss <Ralphsuber@gmail.com>
2024-05-07 02:02:09 -0400

    Initial Commit

---
+++ Avalanche Studios/Avalanche_StudiosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Avalanche_StudiosApp.swift
+//  Avalanche Studios
+//
+//  Created by Rapheal Suber on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct Avalanche_StudiosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
